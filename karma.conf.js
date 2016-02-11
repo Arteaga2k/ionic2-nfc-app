c41db1bb5a2ffdbfcaa215d2c7177a8d52b7c7c5
Michael DESIGAUD <michaeldesigaud@gmail.com>
2016-02-11 16:55:10 +0100

    Fix coverage

--- karma.conf.js
+++ karma.conf.js
@@ -41,13 +41,14 @@ module.exports = function(config) {
 
         // list of files / patterns to load in the browser
         // we are building the test environment in ./spec-bundle.js
-        files: [{ pattern: 'spec-bundle.js', watched: false },{pattern:'app/**/*.html'}],
+        // {pattern:'app/**/*.html'}
+        files: [{ pattern: 'spec-bundle.js', watched: false }],
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-            'spec-bundle.js': ['coverage', 'webpack', 'sourcemap'] ,
-            'app/**/*.html': ['redirect']
+            'spec-bundle.js': ['coverage', 'webpack', 'sourcemap']
+            //'app/**/*.html': ['redirect']
         },
 
         // Webpack Config at ./webpack.test.config.js
