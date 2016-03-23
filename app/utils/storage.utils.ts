0359bd65807b7d7e74fa08d3e4612ddaba63825d
Michael DESIGAUD <michaeldesigaud@gmail.com>
2016-03-23 16:36:18 +0100

    Add jwt authentication + unit tests fixes

--- app/utils/storage.utils.ts
+++ app/utils/storage.utils.ts
@@ -1,11 +1,12 @@
 
 const NFC_APP_TAGS:string = 'NFC-APP-TAGS';
+const NFC_APP_ACCOUNT:string = 'NFC-APP-ACCOUNT';
 const NFC_APP_TOKEN:string = 'NFC-APP-TOKEN';
 
 export class StorageUtils {
 
     static getItem(itemName:string):any {
-        return localStorage.getItem(itemName);
+        return JSON.parse(localStorage.getItem(itemName));
     }
     static hasTags():boolean {
         return !!this.getItem(NFC_APP_TAGS);
@@ -15,22 +16,36 @@ export class StorageUtils {
     }
     static getTags():Array<any> {
         if(this.hasTags()) {
-            return JSON.parse(this.getItem(NFC_APP_TAGS));
+            return this.getItem(NFC_APP_TAGS);
         }
         return [];
     }
     static setTags(tags:Array<any>) {
         localStorage.setItem(NFC_APP_TAGS,JSON.stringify(tags));
     }
-    static getToken():any {
+    static getToken():string {
         if(this.hasToken()) {
-            return JSON.parse(this.getItem(NFC_APP_TOKEN));
+            return this.getItem(NFC_APP_TOKEN);
         }
     }
-    static setToken(token:any):void {
+    static setToken(token:string):void {
         localStorage.setItem(NFC_APP_TOKEN,JSON.stringify(token));
     }
     static removeToken():void {
         localStorage.removeItem(NFC_APP_TOKEN);
+    }
+    static hasAccount():boolean {
+        return !!this.getItem(NFC_APP_ACCOUNT);
+    }
+    static getAccount():any {
+        if(this.hasAccount()) {
+            return this.getItem(NFC_APP_ACCOUNT);
+        }
+    }
+    static setAccount(account:any):void {
+        localStorage.setItem(NFC_APP_ACCOUNT,JSON.stringify(account));
+    }
+    static removeAccount():void {
+        localStorage.removeItem(NFC_APP_ACCOUNT);
     }
 }
